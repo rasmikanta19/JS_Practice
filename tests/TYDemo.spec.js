661d1f20a4a8fbefa791575bf9d6c3baf73aa526
rasmi nayak <rasmi.kanta19@gmail.com>
2025-08-18 13:11:46 +0530

    hela

--- tests/TYDemo.spec.js
+++ tests/TYDemo.spec.js
@@ -2,6 +2,8 @@
 import { test,expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
+
+
   await page.goto('https://tymper.epsilon.com/citiAoCbInt2.htm');
   
   await expect(page).toHaveTitle(/ThankYou/);
@@ -9,6 +11,14 @@ test('has title', async ({ page }) => {
   await page.locator("xpath=//*[@id='leftLogin']/form/div[8]/input").click();
   await page.locator("//*[@id='auth-dd']/span").isVisible();
   await page.locator("//*[@id='auth-dd']/span").click();
-  await browser.close();
+  await page.locator("(//*[@class='account'])[2]/li[2]/a").click();
+  await page.close();
+
+});
+
+test('Another PW Test', async ({browser})=>
+{
+  const context=browser.newContext();
+  browser
 
 });
