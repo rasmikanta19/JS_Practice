203ec3caea14e7bd06624d521415dbd15d6aed01
rasmi nayak <rasmi.kanta19@gmail.com>
2025-08-18 22:08:07 +0530

    code 1

--- tests/TYDemo.spec.js
+++ tests/TYDemo.spec.js
@@ -1,5 +1,6 @@
 
 import { test,expect } from '@playwright/test';
+import { only } from 'node:test';
 
 test('has title', async ({ page }) => {
 
@@ -16,9 +17,19 @@ test('has title', async ({ page }) => {
 
 });
 
-test('Another PW Test', async ({browser})=>
+test.only ('Another PW Test', async ({browser})=>
 {
-  const context=browser.newContext();
-  browser
-
+  const context=await browser.newContext();
+  const page= await context.newPage();
+  await page.goto("https://tymper.epsilon.com/citiAoCbInt2.htm");
+  const NameInitials="//*[@id='auth-dd']/span";
+  await expect(page).toHaveTitle(/ThankYou/);
+  await page.locator("xpath=//*[@id='memberId1']").fill('8910231282192130');
+  await page.locator("xpath=//*[@id='leftLogin']/form/div[8]/input").click();
+  await page.locator(NameInitials).isVisible();
+  await page.locator(NameInitials).click();
+  await page.locator("(//*[@class='account'])[2]/li[2]/a").click();
+  await expect(page).toHaveTitle(/Order History/);
+  await page.locator(NameInitials).click()
+  await page.locator("(//*[@class='account'])[2]/li[3]/a").click();
 });
